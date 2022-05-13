e1fbf44ffefcad0393473663af9aee9591002b71
João Lucas De Souza Paz <joaolucasdesouzapaz1@gmail.com>
2022-05-13 08:07:17 -0300

    array 1

--- Arrays/array1.java
+++ Arrays/array1.java
@@ -3,8 +3,25 @@ package Familia52.Arrays;
 public class array1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+				
+				
+				int num[] = { 1, 0, 5, -2, -5, 7 };
+				int x, soma;
 
-	}
+				soma = num[0] + num[1] + num[5];
+				System.out.println("\nA soma é: " + soma);
 
-}
+				num[4] = 100;
+					
+					
+				for (x = 0; x < num.length; x++) {
+					System.out.println("\nVetor num: \n" + num[x]);
+				}
+
+			}
+
+		}
+
+	
+
+
